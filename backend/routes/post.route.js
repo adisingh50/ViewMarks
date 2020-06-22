cedc97ab57ffd51c53e578707d6a943b9c04b340
Adi Singh <adi.singh901@gmail.com>
2020-06-22 18:18:18 -0400

    finalized backend route for posting pictures

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -20,14 +20,14 @@ router.route("/viewPosts").get((req, res) => {
 });
 
 //post request used to add a view post to the app
-router.route("/addPost").post((req, res) => {
+router.route("/addPost").post(upload.single("image"), (req, res) => {
   const d = new Date();
   const dP = d.toLocaleString("en-US");
 
   const newPost = new Post({
     title: req.body.title,
     description: req.body.description,
-    imagePath: req.body.imagePath,
+    imagePath: req.file.originalname, //make sure you the pass the actual picture file as 'image;
     latitude: req.body.latitude,
     longitude: req.body.longitude,
     numLikes: 0,
@@ -39,11 +39,6 @@ router.route("/addPost").post((req, res) => {
     .save()
     .then(() => res.status(200).json({ added: newPost }))
     .catch((err) => res.status(400).json("Error: " + err));
-});
-
-//image post request - adds image to uploads folder
-router.route("/addImage").post(upload.single("image"), (req, res) => {
-  res.status(200).json({ imagePath: req.file.originalname });
 });
 
 module.exports = router;
